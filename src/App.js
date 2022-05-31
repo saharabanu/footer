de1ff8c8c36c56ab8d36f9d5c55d802f0ab76865
saharabanu <saharabanungn@gmail.com>
2022-05-31 15:49:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <footer className="border">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-5">
+              <h2 className="py-5">Healthport</h2>
+            </div>
+            <div className="col-md-2 py-3">
+              <h4>Our team</h4>
+              <div>
+
+                <p>Team</p>
+                <p>Career</p>
+                <p>Media</p>
+              </div>
+            </div>
+            <div className="col-md-2 py-3">
+              <h4>Hospitals</h4>
+              <div>
+
+                <p>App</p>
+                <p>Website</p>
+                <p>IOT</p>
+              </div>
+            </div>
+            <div className="col-md-3 py-3">
+              <h4>Help</h4>
+              <div>
+
+                <p>FAQ</p>
+                <p>Terms & Conditions</p>
+                <p>Privacy Policy</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </footer>
     </div>
   );
 }
